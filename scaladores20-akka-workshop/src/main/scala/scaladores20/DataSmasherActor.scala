99ee94c19b46c0a57aafd427306fab335c71bf6b
jcranky <paulo.siqueira@gmail.com>
2013-09-18 11:31:25 -0300

    removing try / catch

--- scaladores20-akka-workshop/src/main/scala/scaladores20/DataSmasherActor.scala
+++ scaladores20-akka-workshop/src/main/scala/scaladores20/DataSmasherActor.scala
@@ -12,27 +12,23 @@ class DataSmasherActor extends Actor {
   def receive = {
     case Start =>
       val startTime = System.nanoTime      
-      try {
-        // obviously we need a better way to get the file ... this is just a test scenario
-        // data source: http://www.portaldatransparencia.gov.br/planilhas/index.asp
+      // obviously we need a better way to get the file ... this is just a test scenario
+      // data source: http://www.portaldatransparencia.gov.br/planilhas/index.asp
         
-        val data = Source.fromFile("src/main/resources/TransfRecAcoesEstadoMunicipios-2012.csv", "iso-8859-1")
+      val data = Source.fromFile("src/main/resources/TransfRecAcoesEstadoMunicipios-2012.csv", "iso-8859-1")
         
-        // tail used to remove the headers
-        val dataPerState = data.getLines.toList.tail.groupBy(line => line.split(";")(3))
+      // tail used to remove the headers
+      val dataPerState = data.getLines.toList.tail.groupBy(line => line.split(";")(3))
         
-        val valuePerState = dataPerState.map{
-          case (state, entries) => (state, entries.foldLeft(0d)(
-                (total, line) => total + line.split(";").last.replaceAll(",", ".").toDouble))
-        }
+      val valuePerState = dataPerState.map{
+        case (state, entries) => (state, entries.foldLeft(0d)(
+              (total, line) => total + line.split(";").last.replaceAll(",", ".").toDouble))
+      }
         
-        valuePerState.foreach {
-          case (state, value) =>
-            val formatter = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"))
-            println(s"$state - ${formatter.format(value)}")
-        }
-      } catch {
-        case e: Exception => println(s"problem with file: ${e.getMessage}"); e.printStackTrace
+      valuePerState.foreach {
+        case (state, value) =>
+          val formatter = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"))
+          println(s"$state - ${formatter.format(value)}")
       }
       
       println(s"Finished in ${(System.nanoTime - startTime) / 1E9} seconds")
